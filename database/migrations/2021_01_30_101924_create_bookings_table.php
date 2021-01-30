b213fa0c49886c3fe653c844de25b719644ac855
bigjilm <patera@posteo.de>
2021-01-30 12:02:12 +0100

    Add stub of Booking model

---
+++ database/migrations/2021_01_30_101924_create_bookings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('screening_id');
+            $table->foreign('screening_id')->references('id')->on('screenings');
+            $table->integer('tickets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
