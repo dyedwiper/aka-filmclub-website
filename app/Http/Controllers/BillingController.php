1fd4ba9b0841eee1c707272eff5f89b7aa5a8ca6
bigjilm <patera@posteo.de>
2021-04-11 16:28:21 +0200

    Create billing service and move methods from controller

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -7,11 +7,19 @@ use App\Models\Billing;
 use App\Models\PassStack;
 use App\Models\Screening;
 use App\Models\TicketStack;
+use App\Services\BillingService;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
 
 class BillingController extends Controller
 {
+    private $billingService;
+
+    public function __construct(BillingService $billingService)
+    {
+        $this->billingService = $billingService;
+    }
+
     public function GetScreeningsWithBillingsBySemester(string $semester)
     {
         $season = substr($semester, 0, 2);
@@ -25,7 +33,7 @@ class BillingController extends Controller
                 ->whereMonth('date', '<', 4)
                 ->orderBy('date')
                 ->get();
-            return $screenings->map([$this, 'addBillingFieldsToScreening']);
+            return $screenings->map([$this->billingService, 'addBillingFieldsToScreening']);
         } elseif ($season == 'SS') {
             $screenings = Screening::select('id', 'uuid', 'title', 'date')
                 ->whereYear('date', $year)
@@ -33,7 +41,7 @@ class BillingController extends Controller
                 ->whereMonth('date', '<', 10)
                 ->orderBy('date')
                 ->get();
-            return $screenings->map([$this, 'addBillingFieldsToScreening']);
+            return $screenings->map([$this->billingService, 'addBillingFieldsToScreening']);
         }
     }
 
@@ -44,14 +52,7 @@ class BillingController extends Controller
             ->with('distributor')
             ->with('ticketStacks', 'passStacks')
             ->first();
-        $billing->ticketsCount = $this->calculateTicketsCount($billing);
-        $billing->earnings = $this->calculateEarnings($billing);
-        $billing->ticketEarnings = $this->calculateTicketEarnings($billing);
-        $billing->netTicketEarnings = $this->calculateNetTicketEarnings($billing);
-        $billing->rent = $this->calculateRent($billing);
-        $billing->valueAddedTax = $this->calculateValueAddedTax($billing);
-        $billing->debt = $this->calcaluteDebt($billing);
-        $billing->balance = $this->calculateBalance($billing);
+        $this->billingService->addCalculatedFieldsToBilling($billing);
         return $billing;
     }
 
@@ -161,107 +162,5 @@ class BillingController extends Controller
             $billing->uuid = uniqid();
             $billing->save();
         }
-    }
-
-    //---------------------------------------------//
-    // Below are service methods which don't belong to a route.
-
-    // This method must be public. Otherwise the callback does not work.
-    public function addBillingFieldsToScreening($screening)
-    {
-        if ($screening->billing) {
-            $billing = $screening->billing;
-            $billing->ticketsCount = $this->calculateTicketsCount($billing);
-            $billing->passesCount = $this->calculatePassesCount($billing);
-            $billing->earnings = $this->calculateEarnings($billing);
-            $billing->rent = $this->calculateRent($billing);
-            $billing->balance = $this->calculateBalance($billing);
-            // The ticketStacks and passStacks fields are populated by the calculate methods. Thus they are removed here.
-            // In order to call the forget method, $billing must be first turned into a collection.
-            $billingCollection = collect($billing);
-            $billingCollection->forget('ticketStacks');
-            $billingCollection->forget('passStacks');
-            $screening->billing = $billingCollection;
-        }
-        return $screening;
-    }
-
-    private function calculateTicketsCount($billing)
-    {
-        $ticketStacks = $billing->ticketStacks;
-        $sum = 0;
-        foreach ($ticketStacks as $stack) {
-            $sum += $stack->lastNumber - $stack->firstNumber + 1;
-        }
-        return $sum;
-    }
-
-    private function calculatePassesCount($billing)
-    {
-        $passStacks = $billing->passStacks;
-        $sum = 0;
-        foreach ($passStacks as $stack) {
-            $sum += $stack->lastNumber - $stack->firstNumber + 1;
-        }
-        return $sum;
-    }
-
-    private function calculateTicketEarnings($billing)
-    {
-        $ticketStacks = $billing->ticketStacks;
-        $ticketEarnings = 0;
-        foreach ($ticketStacks as $stack) {
-            $ticketEarnings += ($stack->lastNumber - $stack->firstNumber + 1) * $stack->price;
-        }
-        return $ticketEarnings;
-    }
-
-    private function calculatePassEarnings($billing)
-    {
-        $passStacks = $billing->passStacks;
-        $passEarnings = 0;
-        foreach ($passStacks as $stack) {
-            $passEarnings += ($stack->lastNumber - $stack->firstNumber + 1) * $stack->price;
-        }
-        return $passEarnings;
-    }
-
-    private function calculateEarnings($billing)
-    {
-        return $this->calculateTicketEarnings($billing) + $this->calculatePassEarnings($billing);
-    }
-
-    private function calculateNetTicketEarnings($billing)
-    {
-        return $this->calculateTicketEarnings($billing)
-            - $this->calculateTicketsCount($billing)
-            * Config::get('constants.ticketTax');
-    }
-
-    public function calculateRent($billing)
-    {
-        $earnings = $this->calculateNetTicketEarnings($billing);
-        $rent = $billing->percentage / 100 * $earnings;
-        if ($rent < $billing->guarantee) {
-            $rent = $billing->guarantee;
-        }
-        return $rent;
-    }
-
-    private function calculateBalance($billing)
-    {
-        return $this->calculateTicketEarnings($billing)
-            - $this->calculateRent($billing)
-            - $billing->incidentals;
-    }
-
-    private function calculateValueAddedTax($billing)
-    {
-        return ($this->calculateRent($billing) + $billing->incidentals) * $billing->valueAddedTaxRate / 100;
-    }
-
-    private function calcaluteDebt($billing)
-    {
-        return ($this->calculateRent($billing) + $billing->incidentals) * ($billing->valueAddedTaxRate + 100) / 100;
     }
 }
---
+++ app/Services/BillingService.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
+
+class BillingService
+{
+    public function addBillingFieldsToScreening($screening)
+    {
+        if ($screening->billing) {
+            $this->addCalculatedFieldsToBilling($screening->billing);
+        }
+        return $screening;
+    }
+
+    public function addCalculatedFieldsToBilling($billing)
+    {
+        $billing->ticketsCount = $this->calculateTicketsCount($billing);
+        $billing->earnings = $this->calculateEarnings($billing);
+        $billing->ticketEarnings = $this->calculateTicketEarnings($billing);
+        $billing->netTicketEarnings = $this->calculateNetTicketEarnings($billing);
+        $billing->rent = $this->calculateRent($billing);
+        $billing->valueAddedTax = $this->calculateValueAddedTax($billing);
+        $billing->debt = $this->calcaluteDebt($billing);
+        $billing->balance = $this->calculateBalance($billing);
+    }
+
+    public function calculateTicketsCount($billing)
+    {
+        $ticketStacks = $billing->ticketStacks;
+        $sum = 0;
+        foreach ($ticketStacks as $stack) {
+            $sum += $stack->lastNumber - $stack->firstNumber + 1;
+        }
+        return $sum;
+    }
+
+    public function calculatePassesCount($billing)
+    {
+        $passStacks = $billing->passStacks;
+        $sum = 0;
+        foreach ($passStacks as $stack) {
+            $sum += $stack->lastNumber - $stack->firstNumber + 1;
+        }
+        return $sum;
+    }
+
+    public function calculateTicketEarnings($billing)
+    {
+        $ticketStacks = $billing->ticketStacks;
+        $ticketEarnings = 0;
+        foreach ($ticketStacks as $stack) {
+            $ticketEarnings += ($stack->lastNumber - $stack->firstNumber + 1) * $stack->price;
+        }
+        return $ticketEarnings;
+    }
+
+    public function calculatePassEarnings($billing)
+    {
+        $passStacks = $billing->passStacks;
+        $passEarnings = 0;
+        foreach ($passStacks as $stack) {
+            $passEarnings += ($stack->lastNumber - $stack->firstNumber + 1) * $stack->price;
+        }
+        return $passEarnings;
+    }
+
+    public function calculateEarnings($billing)
+    {
+        return $this->calculateTicketEarnings($billing) + $this->calculatePassEarnings($billing);
+    }
+
+    public function calculateNetTicketEarnings($billing)
+    {
+        return $this->calculateTicketEarnings($billing)
+            - $this->calculateTicketsCount($billing)
+            * Config::get('constants.ticketTax');
+    }
+
+    public function calculateRent($billing)
+    {
+        $earnings = $this->calculateNetTicketEarnings($billing);
+        $rent = $billing->percentage / 100 * $earnings;
+        if ($rent < $billing->guarantee) {
+            $rent = $billing->guarantee;
+        }
+        return $rent;
+    }
+
+    public function calculateBalance($billing)
+    {
+        return $this->calculateTicketEarnings($billing)
+            - $this->calculateRent($billing)
+            - $billing->incidentals;
+    }
+
+    public function calculateValueAddedTax($billing)
+    {
+        return ($this->calculateRent($billing) + $billing->incidentals) * $billing->valueAddedTaxRate / 100;
+    }
+
+    public function calcaluteDebt($billing)
+    {
+        return ($this->calculateRent($billing) + $billing->incidentals) * ($billing->valueAddedTaxRate + 100) / 100;
+    }
+}
