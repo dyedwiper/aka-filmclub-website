f8e27e18d4c36a0d9ce84aacb39a9eb44c154f75
bigjilm <patera@posteo.de>
2021-04-23 22:44:49 +0200

    Build response for screening

--- app/Http/Middleware/SendJustMetaWhenSharing.php
+++ app/Http/Middleware/SendJustMetaWhenSharing.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Screening;
 use App\Utils\PathUtils;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Log;
 
 class SendJustMetaWhenSharing
@@ -18,14 +20,33 @@ class SendJustMetaWhenSharing
      */
     public function handle(Request $request, Closure $next)
     {
+        $baseUrl = Config::get('app.url');
+        $storageBaseUrl = $baseUrl . '/storage/';
+        $akaName = 'aka-Filmclub';
+        $locale = 'de_DE';
+
         $userAgent = $request->header('user-agent');
         $path = $request->path;
-        Log::channel('personal')->debug($path);
-        Log::channel('personal')->debug(PathUtils::getLastSegment($path));
+        // Log::channel('personal')->debug($userAgent);
+        // Log::channel('personal')->debug(PathUtils::getLastSegment($path));
 
         if (str_contains($userAgent, 'facebookexternalhit') || str_contains($userAgent, 'TelegramBot')) {
             if (str_starts_with($path, 'screening')) {
                 $uuid = PathUtils::getLastSegment($path);
+                $screening = Screening::where('uuid', $uuid)->with('image')->first();
+                if (!$screening) {
+                    abort(404, 'Vorführung nicht gefunden.');
+                }
+                $imageUrl = $storageBaseUrl . $screening->image->path;
+                return response("
+                    <meta property=\"og:title\" content=\"$screening->title | $akaName\">
+                    <meta property=\"og:image\" content=\"$imageUrl\">
+                    <meta property=\"og:type\" content=\"website\" />
+                    <meta property=\"og:url\" content=\"$baseUrl/$path\" />
+                    <meta property=\"og:site_name\" content=\"$akaName\" />
+                    <meta property=\"og:description\" content=\"$screening->synopsis\" />
+                    <meta property=\"og:locale\" content=\"$locale\" />
+                ");
             }
         }
 
--- app/Utils/PathUtils.php
+++ app/Utils/PathUtils.php
@@ -6,17 +6,8 @@ use Illuminate\Support\Facades\Log;
 
 class PathUtils
 {
-    const uuidLength = 12;
-
     public static function getLastSegment($path)
     {
-        Log::channel('personal')->debug(strrpos($path, '/'));
-        return substr($path, -strrpos($path, '/'));
-    }
-
-    public static function getUuid($path)
-    {
-        Log::channel('personal')->debug(strrpos($path, '/'));
-        return substr($path, -uuidLength);
+        return substr($path, strrpos($path, '/') + 1);
     }
 }
