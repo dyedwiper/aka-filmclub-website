c93e5e5cd59a101941dc3c97d8b9762d34ddbd6a
bigjilm <patera@posteo.de>
2021-04-24 10:56:52 +0200

    Refactor middleware

--- app/Http/Middleware/SendJustMetaWhenSharing.php
+++ app/Http/Middleware/SendJustMetaWhenSharing.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Log;
 
 class SendJustMetaWhenSharing
 {
-    const PATH_SCREENING = 'screening';
-    const PATH_SERIAL = 'serial';
-    const PATH_NOTICE = 'notice';
+    const ROUTE_SCREENING = 'screening';
+    const ROUTE_SERIAL = 'serial';
+    const ROUTE_NOTICE = 'notice';
+
+    const STORAGE_PATH = '/storage/';
 
     /**
      * Handle an incoming request.
@@ -26,80 +28,111 @@ class SendJustMetaWhenSharing
      */
     public function handle(Request $request, Closure $next)
     {
-        // Use variables instead of constants here because constants can't be expanded easily in strings.
-        $akaName = 'aka-Filmclub';
-        $locale = 'de_DE';
-        $imageUrl = '';
-
-        $baseUrl = Config::get('app.url');
-        $storageBaseUrl = $baseUrl . '/storage/';
-
         $userAgent = $request->header('user-agent');
         $path = $request->path;
         // Log::channel('personal')->debug($userAgent);
         // Log::channel('personal')->debug(PathUtils::getLastSegment($path));
 
         if (str_contains($userAgent, 'facebookexternalhit') || str_contains($userAgent, 'TelegramBot')) {
-            if (str_starts_with($path, self::PATH_SCREENING)) {
-                $uuid = PathUtils::getLastSegment($path);
-                $screening = Screening::where('uuid', $uuid)->with('image')->first();
-                if (!$screening) {
-                    abort(404);
-                }
-                if ($screening->image) {
-                    $imageUrl = $storageBaseUrl . $screening->image->path;
-                }
-                $strippedSynopsis = strip_tags($screening->synopsis);
-                return response("
-                    <meta property=\"og:title\" content=\"$screening->title | $akaName\">
-                    <meta property=\"og:image\" content=\"$imageUrl\">
-                    <meta property=\"og:type\" content=\"website\" />
-                    <meta property=\"og:url\" content=\"$baseUrl/$path\" />
-                    <meta property=\"og:site_name\" content=\"$akaName\" />
-                    <meta property=\"og:description\" content=\"$strippedSynopsis\" />
-                    <meta property=\"og:locale\" content=\"$locale\" />
-                ");
-            } else if (str_starts_with($path, self::PATH_SERIAL)) {
-                $uuid = PathUtils::getLastSegment($path);
-                $serial = Serial::where('uuid', $uuid)->with('image')->first();
-                if (!$serial) {
-                    abort(404);
-                }
-                if ($serial->image) {
-                    $imageUrl = $storageBaseUrl . $serial->image->path;
-                }
-                $strippedArticle = strip_tags($serial->article);
-                return response("
-                    <meta property=\"og:title\" content=\"$serial->title | $akaName\">
-                    <meta property=\"og:image\" content=\"$imageUrl\">
-                    <meta property=\"og:type\" content=\"website\" />
-                    <meta property=\"og:url\" content=\"$baseUrl/$path\" />
-                    <meta property=\"og:site_name\" content=\"$akaName\" />
-                    <meta property=\"og:description\" content=\"$strippedArticle\" />
-                    <meta property=\"og:locale\" content=\"$locale\" />
-                ");
-            } else if (str_starts_with($path, self::PATH_NOTICE)) {
-                $uuid = PathUtils::getLastSegment($path);
-                $notice = Notice::where('uuid', $uuid)->with('image')->first();
-                if (!$notice) {
-                    abort(404);
-                }
-                if ($notice->image) {
-                    $imageUrl = $storageBaseUrl . $notice->image->path;
-                }
-                $strippedContent = strip_tags($notice->content);
-                return response("
-                    <meta property=\"og:title\" content=\"$notice->title | $akaName\">
-                    <meta property=\"og:image\" content=\"$imageUrl\">
-                    <meta property=\"og:type\" content=\"website\" />
-                    <meta property=\"og:url\" content=\"$baseUrl/$path\" />
-                    <meta property=\"og:site_name\" content=\"$akaName\" />
-                    <meta property=\"og:description\" content=\"$strippedContent\" />
-                    <meta property=\"og:locale\" content=\"$locale\" />
-                ");
+            if (str_starts_with($path, self::ROUTE_SCREENING)) {
+                $screeningData = $this->getScreeningData($path);
+                $ogMeta = $this->createOgMeta($screeningData['title'], $screeningData['imageUrl'], $screeningData['description'], $path);
+                return response($ogMeta);
+            } else if (str_starts_with($path, self::ROUTE_SERIAL)) {
+                $serialData = $this->getSerialData($path);
+                $ogMeta = $this->createOgMeta($serialData['title'], $serialData['imageUrl'], $serialData['description'], $path);
+                return response($ogMeta);
+            } else if (str_starts_with($path, self::ROUTE_NOTICE)) {
+                $noticeData = $this->getNoticeData($path);
+                $ogMeta = $this->createOgMeta($noticeData['title'], $noticeData['imageUrl'], $noticeData['description'], $path);
+                return response($ogMeta);
+            }
+        } else if (str_contains($userAgent, 'Twitterbot')) {
+            if (str_starts_with($path, self::ROUTE_SCREENING)) {
             }
         }
 
         return $next($request);
+    }
+
+    private function getScreeningData($path)
+    {
+        $screeningData = [];
+        $baseUrl = Config::get('app.url');
+        $uuid = PathUtils::getLastSegment($path);
+        $screening = Screening::where('uuid', $uuid)->with('image')->first();
+        if (!$screening) {
+            abort(404);
+        }
+        $screeningData['title'] = $screening->title;
+        $screeningData['imageUrl'] = "";
+        if ($screening->image) {
+            $screeningData['imageUrl'] = $baseUrl . self::STORAGE_PATH . $screening->image->path;
+        }
+        $screeningData['description'] = strip_tags($screening->synopsis);
+        return $screeningData;
+    }
+
+    private function getSerialData($path)
+    {
+        $serialData = [];
+        $baseUrl = Config::get('app.url');
+        $uuid = PathUtils::getLastSegment($path);
+        $serial = Serial::where('uuid', $uuid)->with('image')->first();
+        if (!$serial) {
+            abort(404);
+        }
+        $serialData['title'] = $serial->title;
+        $serialData['imageUrl'] = "";
+        if ($serial->image) {
+            $serialData['imageUrl'] = $baseUrl . self::STORAGE_PATH . $serial->image->path;
+        }
+        $serialData['description'] = strip_tags($serial->article);
+        return $serialData;
+    }
+
+    private function getNoticeData($path)
+    {
+        $noticeData = [];
+        $baseUrl = Config::get('app.url');
+        $uuid = PathUtils::getLastSegment($path);
+        $notice = Notice::where('uuid', $uuid)->with('image')->first();
+        if (!$notice) {
+            abort(404);
+        }
+        $noticeData['title'] = $notice->title;
+        $noticeData['imageUrl'] = "";
+        if ($notice->image) {
+            $noticeData['imageUrl'] = $baseUrl . self::STORAGE_PATH . $notice->image->path;
+        }
+        $noticeData['description'] = strip_tags($notice->content);
+        return $noticeData;
+    }
+
+    private function createOgMeta($title, $imageUrl, $description, $path)
+    {
+        $baseUrl = Config::get('app.url');
+        $akaName = 'aka-Filmclub';
+        $locale = 'de_DE';
+
+        return "
+            <meta property=\"og:title\" content=\"$title | $akaName\">
+            <meta property=\"og:image\" content=\"$imageUrl\">
+            <meta property=\"og:type\" content=\"website\" />
+            <meta property=\"og:url\" content=\"$baseUrl/$path\" />
+            <meta property=\"og:site_name\" content=\"$akaName\" />
+            <meta property=\"og:description\" content=\"$description\" />
+            <meta property=\"og:locale\" content=\"$locale\" />
+        ";
+    }
+
+    private function createTwitterMeta($title, $imageUrl, $description)
+    {
+        return "
+            <meta name=\"twitter:card\" content=\"summary_large_image\" />
+            <meta name=\"twitter:title\" content=\"$title\">
+            <meta name=\"twitter:image\" content=\"$imageUrl\">
+            <meta name=\"twitter:description\" content=\"$description\" />
+        ";
     }
 }
