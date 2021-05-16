27b07ab2ff16fc008fb8f20bcd43f1d565a2976a
bigjilm <patera@posteo.de>
2021-05-16 16:50:58 +0200

    Authorize request

--- app/Http/Requests/PasswordFormRequest.php
+++ app/Http/Requests/PasswordFormRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use App\Utils\ValidationUtils;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class PasswordFormRequest extends FormRequest
 {
@@ -15,7 +16,7 @@ class PasswordFormRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return $this->uuid == $this->user()->uuid;
     }
 
     /**
