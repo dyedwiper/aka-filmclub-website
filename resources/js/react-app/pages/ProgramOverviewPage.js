e7a6d15586546f72ac0ad5b6b10ac1ba6d9fcdc0
bigjilm <patera@posteo.de>
2021-06-11 15:06:13 +0200

    Add link to ProgramOverview to AddScreeningPage

--- resources/js/react-app/pages/ProgramOverviewPage.js
+++ resources/js/react-app/pages/ProgramOverviewPage.js
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import BasePage from '../common/BasePage';
 import CalendarSeriesDownloadLink from '../common/calendar/CalendarSeriesDownloadLink';
 import ScreeningsListItem from '../common/screenings/ScreeningsListItem';
-import { PageHeadlineStyled } from '../common/styledElements';
-import { PAGE_TITLE_PROGRAM_OVERVIEW } from '../constants';
+import { AddItemLinkStyled, PageHeadlineStyled } from '../common/styledElements';
+import { AUTH_LEVEL_EDITOR, PAGE_TITLE_PROGRAM_OVERVIEW, ROUTE_INTERN_ADD_SCREENING } from '../constants';
+import Context from '../Context';
 import { getFutureScreenings } from '../utils/services/screeningServices';
 
 export default function ProgramOverviewPage() {
     const [screenings, setScreenings] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+
+    const { user } = useContext(Context);
+    const isAuthorized = user.level >= AUTH_LEVEL_EDITOR;
 
     useEffect(() => {
         getFutureScreenings().then((res) => {
@@ -21,20 +25,27 @@ export default function ProgramOverviewPage() {
     return (
         <BasePage pageTitle={PAGE_TITLE_PROGRAM_OVERVIEW}>
             <PageHeadlineStyled>{PAGE_TITLE_PROGRAM_OVERVIEW}</PageHeadlineStyled>
+            {isAuthorized && (
+                <AddItemLinkStyled to={ROUTE_INTERN_ADD_SCREENING}>Vorführung hinzufügen</AddItemLinkStyled>
+            )}
             {isLoading ? (
                 <div>Loading</div>
             ) : (
-                <>
+                <ContainerStyled>
                     {screenings.length > 0 && <CalendarSeriesDownloadLink screenings={screenings} />}
                     <ScreeningsListStyled>
                         {screenings.map((screening) => (
                             <ScreeningsListItem key={screening.id} screening={screening} />
                         ))}
                     </ScreeningsListStyled>
-                </>
+                </ContainerStyled>
             )}
         </BasePage>
     );
 }
+
+const ContainerStyled = styled.div`
+    margin-top: 20px;
+`;
 
 const ScreeningsListStyled = styled.ul``;
