7cbd5ec28a46c1707d86f89a5afb4ff84d8603e4
bigjilm <patera@posteo.de>
2021-08-22 13:14:01 +0200

    Update favicons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,15 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
     <script src="/js/app.js" defer></script>
     <title>aka-Filmclub</title>
+
+    <!-- Below some links for favicons. See README_favicons.md in public folder for more information. -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffef14">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="theme-color" content="#ffef14">
 </head>
 
 <body>
