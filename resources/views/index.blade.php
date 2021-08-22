712b3be1e4af19b96fda9be120d757593639fd60
bigjilm <patera@posteo.de>
2021-08-22 16:36:05 +0200

    Embed font

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
     <script src="/js/app.js" defer></script>
     <title>aka-Filmclub</title>
 
