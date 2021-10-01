cf534f8f5e13c4aa8db12d1b1c527abaa2786cf1
bigjilm <patera@posteo.de>
2021-10-01 16:02:44 +0200

    Fix authorization in AddUserPage

--- resources/js/react-app/pages/intern/AddUserPage.js
+++ resources/js/react-app/pages/intern/AddUserPage.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import BasePage from '../../common/BasePage';
 import BaseForm from '../../common/forms/BaseForm';
 import UserFormGroup from '../../common/forms/UserFormGroup';
 import { PAGE_TITLE_ADD_USER, ROUTE_INTERN_USERS } from '../../constants';
+import Context from '../../Context';
 import { postUser } from '../../utils/services/userServices';
 
 export default function AddUserPage() {
+    const { isUserAdmin } = useContext(Context);
+
     return (
         <BasePage pageTitle={PAGE_TITLE_ADD_USER}>
             <HeadlineStyled>{PAGE_TITLE_ADD_USER}</HeadlineStyled>
             <BaseForm postFunction={postUser} postRedirectRoute={ROUTE_INTERN_USERS}>
-                <UserFormGroup />
+                <UserFormGroup isUserAdmin={isUserAdmin} isAuthorized={isUserAdmin} />
             </BaseForm>
         </BasePage>
     );
