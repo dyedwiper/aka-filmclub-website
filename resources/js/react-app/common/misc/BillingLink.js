a55f13bc7774285a64529666d625ecff4698599c
bigjilm <patera@posteo.de>
2022-11-06 19:59:19 +0100

    Fix billing link on screening page

--- resources/js/react-app/common/misc/BillingLink.js
+++ resources/js/react-app/common/misc/BillingLink.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ROUTE_INTERN_ADD_BILLING, ROUTE_INTERN_EDIT_BILLING } from '../../constants';
+import { ROUTE_INTERN_ADD_BILLING, ROUTE_INTERN_BILLING } from '../../constants';
 import { VerticalLineStyled } from '../styledElements';
 
 export default function BillingLink({ screening }) {
@@ -10,7 +10,7 @@ export default function BillingLink({ screening }) {
         <>
             <VerticalLineStyled> | </VerticalLineStyled>
             {screening.billing ? (
-                <Link to={ROUTE_INTERN_EDIT_BILLING + screening.billing.uuid}>Abrechnung bearbeiten</Link>
+                <Link to={ROUTE_INTERN_BILLING + screening.billing.uuid}>Abrechnung ansehen</Link>
             ) : (
                 <Link to={ROUTE_INTERN_ADD_BILLING + screening.uuid}>Abrechnung hinzufügen</Link>
             )}
