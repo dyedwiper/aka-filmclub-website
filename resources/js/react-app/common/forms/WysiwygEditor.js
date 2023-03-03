3722ff471781ae3fb09e242b5afd6f83cb303660
dyedwiper <patera@posteo.de>
2023-03-03 12:24:56 +0100

    Make image uploadable in all editors

--- resources/js/react-app/common/forms/WysiwygEditor.js
+++ resources/js/react-app/common/forms/WysiwygEditor.js
@@ -1,15 +1,17 @@
-import { ContentState, convertFromHTML, convertToRaw, EditorState } from 'draft-js';
+import { ContentState, convertToRaw, EditorState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
 import React, { useState } from 'react';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import styled from 'styled-components';
 import { editorStyleObject, toolbarStyleObject, wrapperStyleObject } from '../../styles/wysisygEditorStyles';
+import { postImageFromWysiwygEditor } from '../../utils/services/imageServices';
 
 export default function WysiwygEditor({ inputName, defaultValue }) {
     const [editorState, setEditorState] = useState(() =>
         defaultValue
-            ? EditorState.createWithContent(ContentState.createFromBlockArray(convertFromHTML(defaultValue)))
+            ? EditorState.createWithContent(ContentState.createFromBlockArray(htmlToDraft(defaultValue)))
             : EditorState.createEmpty()
     );
 
@@ -22,7 +24,7 @@ export default function WysiwygEditor({ inputName, defaultValue }) {
                 toolbarStyle={toolbarStyleObject}
                 editorStyle={editorStyleObject}
                 toolbar={{
-                    options: ['inline', 'link'],
+                    options: ['inline', 'link', 'image'],
                     inline: {
                         options: ['bold', 'italic', 'underline', 'strikethrough'],
                     },
@@ -30,11 +32,32 @@ export default function WysiwygEditor({ inputName, defaultValue }) {
                         showOpenOptionOnHover: false,
                         defaultTargetOption: '_blank',
                     },
+                    image: {
+                        uploadCallback: uploadImage,
+                        previewImage: true,
+                        inputAccept: 'image/gif,image/jpeg,image/jpg,image/png',
+                        alt: { present: true, mandatory: false },
+                    },
                 }}
             />
             <input type="hidden" name={inputName} value={draftToHtml(convertToRaw(editorState.getCurrentContent()))} />
         </WysiwygEditorStyled>
     );
+
+    function uploadImage(image) {
+        const formData = new FormData();
+        formData.append('image', image);
+        return postImageFromWysiwygEditor(formData)
+            .then((res) => {
+                return { data: { link: res.data } };
+            })
+            .catch((err) => {
+                if (err.response.status === 422) {
+                    // setValidationErrors(err.response.data.validationErrors);
+                    console.log(err.response.data.validationErrors);
+                }
+            });
+    }
 }
 
 const WysiwygEditorStyled = styled.div``;
