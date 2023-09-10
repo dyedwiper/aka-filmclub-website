88087d2afd61a62f92a1a2636f18c77058c63e47
dyedwiper <patera@posteo.de>
2023-09-10 11:32:00 +0200

    Format all php files in routes

--- routes/api.php
+++ routes/api.php
@@ -26,283 +26,158 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('screenings')->group(function () {
+    Route::get('/', [ScreeningController::class, 'GetScreenings']);
 
-    Route::get('/', [
-        ScreeningController::class, 'GetScreenings'
-    ]);
+    Route::get('/future', [ScreeningController::class, 'GetFutureScreenings']);
 
-    Route::get('/future', [
-        ScreeningController::class, 'GetFutureScreenings'
-    ]);
+    Route::get('/uuid/{uuid}', [ScreeningController::class, 'GetScreeningByUuid']);
 
-    Route::get('/uuid/{uuid}', [
-        ScreeningController::class, 'GetScreeningByUuid'
-    ]);
+    Route::get('semester/{semester}', [ScreeningController::class, 'GetScreeningsBySemester']);
 
-    Route::get('semester/{semester}', [
-        ScreeningController::class, 'GetScreeningsBySemester'
-    ]);
+    Route::get('search/{search}', [ScreeningController::class, 'GetScreeningsBySearchString']);
 
-    Route::get('search/{search}', [
-        ScreeningController::class, 'GetScreeningsBySearchString'
-    ]);
+    Route::middleware('auth:sanctum')->post('/', [ScreeningController::class, 'PostScreening']);
 
-    Route::middleware('auth:sanctum')->post('/', [
-        ScreeningController::class, 'PostScreening'
-    ]);
+    Route::middleware('auth:sanctum')->patch('/', [ScreeningController::class, 'PatchScreening']);
 
-    Route::middleware('auth:sanctum')->patch('/', [
-        ScreeningController::class, 'PatchScreening'
-    ]);
-
-    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [
-        ScreeningController::class, 'DeleteScreening'
-    ]);
+    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [ScreeningController::class, 'DeleteScreening']);
 });
 
 Route::prefix('notices')->group(function () {
-    Route::get('/', [
-        NoticeController::class, 'GetNotices'
-    ]);
+    Route::get('/', [NoticeController::class, 'GetNotices']);
 
-    Route::get('/count', [
-        NoticeController::class, 'GetNoticesCount'
-    ]);
+    Route::get('/count', [NoticeController::class, 'GetNoticesCount']);
 
-    Route::get('/uuid/{uuid}', [
-        NoticeController::class, 'GetNoticeByUuid'
-    ]);
+    Route::get('/uuid/{uuid}', [NoticeController::class, 'GetNoticeByUuid']);
 
-    Route::middleware('auth:sanctum')->post('/', [
-        NoticeController::class, 'PostNotice'
-    ]);
+    Route::middleware('auth:sanctum')->post('/', [NoticeController::class, 'PostNotice']);
 
-    Route::middleware('auth:sanctum')->patch('/', [
-        NoticeController::class, 'PatchNotice'
-    ]);
+    Route::middleware('auth:sanctum')->patch('/', [NoticeController::class, 'PatchNotice']);
 
-    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [
-        NoticeController::class, 'DeleteNotice'
-    ]);
+    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [NoticeController::class, 'DeleteNotice']);
 });
 
 Route::prefix('serials')->group(function () {
-    Route::get('/', [
-        SerialController::class, 'GetSerials'
-    ]);
+    Route::get('/', [SerialController::class, 'GetSerials']);
 
-    Route::get('/future', [
-        SerialController::class, 'GetCurrentAndFutureSerials'
-    ]);
+    Route::get('/future', [SerialController::class, 'GetCurrentAndFutureSerials']);
 
-    Route::get('/semester/{semester}', [
-        SerialController::class, 'GetSerialsBySemester'
-    ]);
+    Route::get('/semester/{semester}', [SerialController::class, 'GetSerialsBySemester']);
 
-    Route::get('/uuid/{uuid}', [
-        SerialController::class, 'GetSerialByUuid'
-    ]);
+    Route::get('/uuid/{uuid}', [SerialController::class, 'GetSerialByUuid']);
 
-    Route::get('search/{search}', [
-        SerialController::class, 'GetSerialsBySearchString'
-    ]);
+    Route::get('search/{search}', [SerialController::class, 'GetSerialsBySearchString']);
 
-    Route::middleware('auth:sanctum')->post('/', [
-        SerialController::class, 'PostSerial'
-    ]);
+    Route::middleware('auth:sanctum')->post('/', [SerialController::class, 'PostSerial']);
 
-    Route::middleware('auth:sanctum')->patch('/', [
-        SerialController::class, 'PatchSerial'
-    ]);
+    Route::middleware('auth:sanctum')->patch('/', [SerialController::class, 'PatchSerial']);
 
-    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [
-        SerialController::class, 'DeleteSerial'
-    ]);
+    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [SerialController::class, 'DeleteSerial']);
 });
 
 Route::prefix('images')->group(function () {
-    Route::get('/uuid/{uuid}', [
-        ImageController::class, 'GetImageByUuid'
-    ]);
+    Route::get('/uuid/{uuid}', [ImageController::class, 'GetImageByUuid']);
 
-    Route::middleware('auth:sanctum')->post('/', [
-        ImageController::class, 'PostImage'
-    ]);
+    Route::middleware('auth:sanctum')->post('/', [ImageController::class, 'PostImage']);
 
-    Route::middleware('auth:sanctum')->patch('/', [
-        ImageController::class, 'PatchImage'
-    ]);
+    Route::middleware('auth:sanctum')->patch('/', [ImageController::class, 'PatchImage']);
 
-    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [
-        ImageController::class, 'DeleteImage'
-    ]);
+    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [ImageController::class, 'DeleteImage']);
 
-    Route::middleware('auth:sanctum')->post('/wysiwyg', [
-        ImageController::class, 'PostImageFromWysiwygEditor'
-    ]);
+    Route::middleware('auth:sanctum')->post('/wysiwyg', [ImageController::class, 'PostImageFromWysiwygEditor']);
 });
 
 Route::prefix('users')->group(function () {
-    Route::middleware('auth:sanctum')->get('/', [
-        UserController::class, 'GetUsers'
-    ]);
+    Route::middleware('auth:sanctum')->get('/', [UserController::class, 'GetUsers']);
 
-    Route::middleware('auth:sanctum')->get('/phpbb', [
-        UserController::class, 'GetPhpbbUsers'
-    ]);
+    Route::middleware('auth:sanctum')->get('/phpbb', [UserController::class, 'GetPhpbbUsers']);
 
-    Route::get('/currentUser', [
-        UserController::class, 'GetCurrentUser'
-    ]);
+    Route::get('/currentUser', [UserController::class, 'GetCurrentUser']);
 
-    Route::middleware('auth:sanctum')->get('/uuid/{uuid}', [
-        UserController::class, 'GetUserByUuid'
-    ]);
+    Route::middleware('auth:sanctum')->get('/uuid/{uuid}', [UserController::class, 'GetUserByUuid']);
 
-    Route::post('/login', [
-        UserController::class, 'PostLogin'
-    ]);
+    Route::post('/login', [UserController::class, 'PostLogin']);
 
-    Route::get('/logout', [
-        UserController::class, 'GetLogout'
-    ]);
-    Route::middleware('auth:sanctum')->post('/', [
-        UserController::class, 'PostUser'
-    ]);
+    Route::get('/logout', [UserController::class, 'GetLogout']);
+    Route::middleware('auth:sanctum')->post('/', [UserController::class, 'PostUser']);
 
-    Route::middleware('auth:sanctum')->patch('/', [
-        UserController::class, 'PatchUser'
-    ]);
+    Route::middleware('auth:sanctum')->patch('/', [UserController::class, 'PatchUser']);
 
-    Route::middleware('auth:sanctum')->patch('/password', [
-        UserController::class, 'PatchPassword'
-    ]);
+    Route::middleware('auth:sanctum')->patch('/password', [UserController::class, 'PatchPassword']);
 
-    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [
-        UserController::class, 'DeleteUser'
-    ]);
+    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [UserController::class, 'DeleteUser']);
 
-    Route::middleware('auth:sanctum')->get('/update_uuids', [
-        UserController::class, 'UpdateUuids'
-    ]);
+    Route::middleware('auth:sanctum')->get('/update_uuids', [UserController::class, 'UpdateUuids']);
 });
 
 Route::prefix('faqs')->group(function () {
-    Route::get('/', [
-        FaqController::class, 'GetFaqs'
-    ]);
+    Route::get('/', [FaqController::class, 'GetFaqs']);
 
-    Route::get('/uuid/{uuid}', [
-        FaqController::class, 'GetFaqByUuid'
-    ]);
+    Route::get('/uuid/{uuid}', [FaqController::class, 'GetFaqByUuid']);
 
-    Route::middleware('auth:sanctum')->post('/', [
-        FaqController::class, 'PostFaq'
-    ]);
+    Route::middleware('auth:sanctum')->post('/', [FaqController::class, 'PostFaq']);
 
-    Route::middleware('auth:sanctum')->patch('/', [
-        FaqController::class, 'PatchFaq'
-    ]);
+    Route::middleware('auth:sanctum')->patch('/', [FaqController::class, 'PatchFaq']);
 
-    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [
-        FaqController::class, 'DeleteFaq'
-    ]);
+    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [FaqController::class, 'DeleteFaq']);
 });
 
 Route::prefix('selfmadeFilms')->group(function () {
-    Route::get('/', [
-        SelfmadeFilmController::class, 'GetSelfmadeFilms'
-    ]);
+    Route::get('/', [SelfmadeFilmController::class, 'GetSelfmadeFilms']);
 
-    Route::get('/uuid/{uuid}', [
-        SelfmadeFilmController::class, 'GetSelfmadeFilmByUuid'
-    ]);
+    Route::get('/uuid/{uuid}', [SelfmadeFilmController::class, 'GetSelfmadeFilmByUuid']);
 
-    Route::middleware('auth:sanctum')->post('/', [
-        SelfmadeFilmController::class, 'PostSelfmadeFilm'
-    ]);
+    Route::middleware('auth:sanctum')->post('/', [SelfmadeFilmController::class, 'PostSelfmadeFilm']);
 
-    Route::middleware('auth:sanctum')->patch('/', [
-        SelfmadeFilmController::class, 'PatchSelfmadeFilm'
-    ]);
+    Route::middleware('auth:sanctum')->patch('/', [SelfmadeFilmController::class, 'PatchSelfmadeFilm']);
 
-    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [
-        SelfmadeFilmController::class, 'DeleteSelfmadeFilm'
-    ]);
+    Route::middleware('auth:sanctum')->delete('/uuid/{uuid}', [SelfmadeFilmController::class, 'DeleteSelfmadeFilm']);
 });
 
 Route::prefix('texts')->group(function () {
-    Route::get('/{page}', [
-        TextController::class, 'GetText'
-    ]);
+    Route::get('/{page}', [TextController::class, 'GetText']);
 
-    Route::middleware('auth:sanctum')->post('/{page}', [
-        TextController::class, 'PostText'
-    ]);
+    Route::middleware('auth:sanctum')->post('/{page}', [TextController::class, 'PostText']);
 });
 
-Route::middleware('auth:sanctum')->prefix('distributors')->group(function () {
-    Route::get('/', [
-        DistributorController::class, 'GetDistributors'
-    ]);
+Route::middleware('auth:sanctum')
+    ->prefix('distributors')
+    ->group(function () {
+        Route::get('/', [DistributorController::class, 'GetDistributors']);
 
-    Route::get('/uuid/{uuid}', [
-        DistributorController::class, 'GetDistributorByUuid'
-    ]);
+        Route::get('/uuid/{uuid}', [DistributorController::class, 'GetDistributorByUuid']);
 
-    Route::post('/', [
-        DistributorController::class, 'PostDistributor'
-    ]);
+        Route::post('/', [DistributorController::class, 'PostDistributor']);
 
-    Route::patch('/', [
-        DistributorController::class, 'PatchDistributor'
-    ]);
+        Route::patch('/', [DistributorController::class, 'PatchDistributor']);
 
-    Route::delete('/uuid/{uuid}', [
-        DistributorController::class, 'DeleteDistributor'
-    ]);
-});
+        Route::delete('/uuid/{uuid}', [DistributorController::class, 'DeleteDistributor']);
+    });
 
-Route::middleware('auth:sanctum')->prefix('billings')->group(function () {
-    Route::get('/semester/{semester}', [
-        BillingController::class, 'GetScreeningsWithBillingsBySemester'
-    ]);
+Route::middleware('auth:sanctum')
+    ->prefix('billings')
+    ->group(function () {
+        Route::get('/semester/{semester}', [BillingController::class, 'GetScreeningsWithBillingsBySemester']);
 
-    Route::get('/uuid/{uuid}', [
-        BillingController::class, 'GetBillingByUuid'
-    ]);
+        Route::get('/uuid/{uuid}', [BillingController::class, 'GetBillingByUuid']);
 
-    Route::post('/', [
-        BillingController::class, 'PostBilling'
-    ]);
+        Route::post('/', [BillingController::class, 'PostBilling']);
 
-    Route::patch('/', [
-        BillingController::class, 'PatchBilling'
-    ]);
+        Route::patch('/', [BillingController::class, 'PatchBilling']);
 
-    Route::delete('/uuid/{uuid}', [
-        BillingController::class, 'DeleteBilling'
-    ]);
-});
+        Route::delete('/uuid/{uuid}', [BillingController::class, 'DeleteBilling']);
+    });
 
-Route::middleware('auth:sanctum')->prefix('licenses')->group(function () {
-    Route::get('/', [
-        LicenseController::class, 'GetLicenses'
-    ]);
+Route::middleware('auth:sanctum')
+    ->prefix('licenses')
+    ->group(function () {
+        Route::get('/', [LicenseController::class, 'GetLicenses']);
 
-    Route::get('/uuid/{uuid}', [
-        LicenseController::class, 'GetLicenseByUuid'
-    ]);
+        Route::get('/uuid/{uuid}', [LicenseController::class, 'GetLicenseByUuid']);
 
-    Route::post('/', [
-        LicenseController::class, 'PostLicense'
-    ]);
+        Route::post('/', [LicenseController::class, 'PostLicense']);
 
-    Route::patch('/', [
-        LicenseController::class, 'PatchLicense'
-    ]);
+        Route::patch('/', [LicenseController::class, 'PatchLicense']);
 
-    Route::delete('/uuid/{uuid}', [
-        LicenseController::class, 'DeleteLicense'
-    ]);
-});
+        Route::delete('/uuid/{uuid}', [LicenseController::class, 'DeleteLicense']);
+    });
