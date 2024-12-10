ddb1b7d583af81acda7a0888bfb1c088de210244
dyedwiper <patera@posteo.de>
2024-12-10 11:12:19 +0100

    Remove dots from currency strings

--- resources/js/react-app/services/billingServices.js
+++ resources/js/react-app/services/billingServices.js
@@ -1,3 +1,4 @@
+import { removeDots } from '../utils/stringUtils';
 import { makeApiCall } from './baseService';
 
 export function getScreeningsWithBillingsBySemester(semester) {
@@ -9,6 +10,20 @@ export function getBillingByUuid(uuid) {
 }
 
 export function postBilling(data) {
+    // Remove dots from localized currency strings to make them processable for the backend
+    const fieldsToRemoveDotsFrom = [
+        'guarantee',
+        'incidentals',
+        'cashInlay',
+        'cashOut',
+        'additionalEarnings',
+        'additionalExpenses',
+    ];
+    fieldsToRemoveDotsFrom.forEach((field) => {
+        const value = data.get(field);
+        data.set(field, removeDots(value));
+    });
+
     return makeApiCall('/api/billings', 'POST', data);
 }
 
