be67ce7352765358891b39d6411ac2afc92c0eee
dyedwiper <patera@posteo.de>
2025-06-14 12:31:35 +0200

    Display contact infos conditionally

--- resources/js/react-app/common/accounting/pdf/AddressHeader.js
+++ resources/js/react-app/common/accounting/pdf/AddressHeader.js
@@ -6,11 +6,26 @@ export default function AddressHeader({ distributor }) {
         <View style={styles.header}>
             <View style={styles.addressContainer}>
                 <Text>{distributor.name}</Text>
-                <Text>{distributor.address}</Text>
-                <Text>{distributor.zipcode + ' ' + distributor.city}</Text>
+                {!!distributor.address && <Text>{distributor.address}</Text>}
+                <Text>
+                    {!!distributor.zipcode && <Text>{distributor.zipcode} </Text>}
+                    {!!distributor.city && <Text>{distributor.city}</Text>}
+                </Text>
             </View>
             <Text>
-                {'E-Mail: ' + distributor.email + ' / Tel: ' + distributor.phone + ' / Fax: ' + distributor.fax}
+                {!!distributor.email && (
+                    <Text>
+                        <Text>{'E-Mail: ' + distributor.email}</Text>
+                        <Text> / </Text>
+                    </Text>
+                )}
+                {!!distributor.phone && (
+                    <Text>
+                        <Text>{'Tel: ' + distributor.phone}</Text>
+                        <Text> / </Text>
+                    </Text>
+                )}
+                {!!distributor.fax && <Text>{'Fax: ' + distributor.fax}</Text>}
             </Text>
         </View>
     );
