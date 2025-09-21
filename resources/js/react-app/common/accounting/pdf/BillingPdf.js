4fcebdd586420499aa66524641e8406ddc508cdb
dyedwiper <patera@posteo.de>
2025-09-21 15:44:10 +0200

    Fix BillingPdf

--- resources/js/react-app/common/accounting/pdf/BillingPdf.js
+++ resources/js/react-app/common/accounting/pdf/BillingPdf.js
@@ -23,39 +23,37 @@ import TicketsTable from './TicketsTable';
 export default function BillingPdf({ billing }) {
     return (
         <Document>
-            <Page size="A4">
-                <View style={styles.pageContainer}>
+            <Page size="A4" style={styles.pageContainer}>
+                <View style={styles.logoContainer}>
                     <Image src={akaLogo} style={styles.logo} />
-                    {billing.distributor && <AddressHeader distributor={billing.distributor} />}
-                    <View style={styles.main}>
-                        <Text style={styles.headline}>Abrechnung: {billing.screening.title}</Text>
-                        <Text style={styles.date}>Spieltermin: {formatToDateString(billing.screening.date)}</Text>
-                        <View style={styles.billingInfo}>
-                            <Text style={styles.infoField}>TB-Nr.: {billing.confirmationNumber}</Text>
-                            <Text style={styles.infoField}>
-                                Prozentsatz: {toGermanString(billing.percentage) + ' %'}
+                </View>
+                {billing.distributor && <AddressHeader distributor={billing.distributor} />}
+                <View style={styles.main}>
+                    <Text style={styles.headline}>Abrechnung: {billing.screening.title}</Text>
+                    <Text style={styles.date}>Spieltermin: {formatToDateString(billing.screening.date)}</Text>
+                    <View style={styles.billingInfo}>
+                        <Text style={styles.infoField}>TB-Nr.: {billing.confirmationNumber}</Text>
+                        <Text style={styles.infoField}>Prozentsatz: {toGermanString(billing.percentage) + ' %'}</Text>
+                        <Text style={styles.infoField}>Unsere Kundennr.: {billing.distributor?.customerId}</Text>
+                        <Text style={styles.infoField}>Mindestgarantie: {toEuroWithSymbol(billing.guarantee)}</Text>
+                    </View>
+                    <TicketsTable billing={billing} />
+                    <AggregationTable billing={billing} />
+                    {billing.distributor?.iban && (
+                        <View style={styles.transferralContainer}>
+                            <Text>Den Betrag überweisen wir in den nächsten Tagen auf folgendes Konto:</Text>
+                            <Text>
+                                IBAN {billing.distributor.iban}
+                                {billing.distributor.bic && ', BIC ' + billing.distributor.bic}
                             </Text>
-                            <Text style={styles.infoField}>Unsere Kundennr.: {billing.distributor?.customerId}</Text>
-                            <Text style={styles.infoField}>Mindestgarantie: {toEuroWithSymbol(billing.guarantee)}</Text>
                         </View>
-                        <TicketsTable billing={billing} />
-                        <AggregationTable billing={billing} />
-                        {billing.distributor?.iban && (
-                            <View style={styles.transferralContainer}>
-                                <Text>Den Betrag überweisen wir in den nächsten Tagen auf folgendes Konto:</Text>
-                                <Text>
-                                    IBAN {billing.distributor.iban}
-                                    {billing.distributor.bic && ', BIC ' + billing.distributor.bic}
-                                </Text>
-                            </View>
-                        )}
-                    </View>
-                    <View style={styles.footer}>
-                        <Text style={styles.akaAddress}>
-                            {AKA_SHORTNAME}, {AKA_ADDRESS}, {AKA_ZIPCODE} {AKA_CITY} | E-Mail: {AKA_EMAIL} | Tel:{' '}
-                            {AKA_PHONE} | Fax: {AKA_FAX} | IBAN: {AKA_IBAN}, BIC: {AKA_BIC}, {AKA_BANK}
-                        </Text>
-                    </View>
+                    )}
+                </View>
+                <View style={styles.footer}>
+                    <Text style={styles.akaAddress}>
+                        {AKA_SHORTNAME}, {AKA_ADDRESS}, {AKA_ZIPCODE} {AKA_CITY} | E-Mail: {AKA_EMAIL} | Tel:{' '}
+                        {AKA_PHONE} | Fax: {AKA_FAX} | IBAN: {AKA_IBAN}, BIC: {AKA_BIC}, {AKA_BANK}
+                    </Text>
                 </View>
             </Page>
         </Document>
@@ -64,13 +62,14 @@ export default function BillingPdf({ billing }) {
 
 const styles = StyleSheet.create({
     pageContainer: { position: 'relative', height: '100vh', padding: '40pt 60pt', fontSize: '12pt' },
-    logo: { position: 'fixed', top: 0, left: '330pt', height: '90pt', width: '120pt' },
+    logoContainer: { display: 'flex', flexDirection: 'row-reverse' },
+    logo: { height: '90pt', width: '120pt' },
     main: { margin: '40pt 0 0 0' },
     headline: { marginBottom: '10pt', fontSize: '24pt' },
     date: { marginBottom: '10pt' },
     billingInfo: { display: 'flex', flexDirection: 'row', flexWrap: 'wrap' },
     infoField: { width: '50%', marginBottom: '10pt' },
     transferralContainer: { margin: '30pt 0' },
-    footer: { position: 'absolute', bottom: 0, width: '100vw', padding: '40pt 60pt' },
+    footer: { position: 'absolute', left: 0, bottom: 0, width: '100vw', padding: '40pt 60pt' },
     akaAddress: { fontSize: '9pt' },
 });
