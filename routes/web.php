9498eab6ee3d8dfd18624ba1a5b9915dfdc8bfcc
dyedwiper <patera@posteo.de>
2025-11-16 15:04:46 +0100

    Remove separate route for files

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('files/{filename}', function ($filename) {
-    return response()->file($filename);
-});
-
 Route::get('/{path}', function () {
     return view('index');
 })->where('path', '^(?!api).*');
